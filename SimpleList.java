45e1e27511719b8c7aeff1e6de1d7d323fc74c9e
jiendra <jitendra@banshkar.com>
2022-09-11 05:12:07 +0530

    UC_6

--- SimpleList.java
+++ SimpleList.java
@@ -5,12 +5,11 @@ public class SimpleList {
         Node<Integer> firstNode = new Node<>(56);
         Node<Integer> secondNode = new Node<>(30);
         Node<Integer> thirdNode = new Node<>(70);
-        Node<Integer> thirdNode1 = new Node<>(170);
         list.insertNodeInBeginning(firstNode);
         list.insertNodeAtEnd(secondNode);
         list.InsertNodeMiddle(thirdNode);
         list.display();
-        list.deletedNodeLast();
+        list.deleteNodeFromBeginning();
         list.display();
     }
 }
