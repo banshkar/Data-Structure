b604faa7fac4409248499c2bef41ecefd279d0c1
jiendra <jitendra@banshkar.com>
2022-09-10 19:02:39 +0530

    Uc_3

--- SimpleList.java
+++ SimpleList.java
@@ -2,9 +2,9 @@ package CreateList;
 public class SimpleList {
     public static void main(String[] args) {
         ListCreate list = new ListCreate();
-        Node<Integer> firstNode = new Node<>(70);
+        Node<Integer> firstNode = new Node<>(56);
         Node<Integer> secondNode = new Node<>(30);
-        Node<Integer> thirdNode = new Node<>(56);
+        Node<Integer> thirdNode = new Node<>(70);
         INode head;
         head =list.insertNode(firstNode);
         head =list.insertNode(secondNode);
